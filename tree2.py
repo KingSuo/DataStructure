463f62741a9f75598b66b648e7abef2d7239a6fb
KingSuo <648230502@qq.com>
2018-06-26 16:56:46 +0800

    fixed error

--- tree2.py
+++ tree2.py
@@ -8,17 +8,17 @@ def insert_node(value, tree={}):
     if tree:
         for node in tree:
             if value < node:
-                if tree[node][0] is None:
-                    tree[node][0] = {value: [None, None]}
+                if not tree[node][0]:
+                    tree[node][0] = {value: [{}, {}]}
                 else:
                     insert_node(value, tree[node][0])
             elif value > node:
-                if tree[node][1] is None:
-                    tree[node][1] = {value: [None, None]}
+                if not tree[node][1]:
+                    tree[node][1] = {value: [{}, {}]}
                 else:
                     insert_node(value, tree[node][1])
     else:
-        tree[value] = [None, None]
+        tree[value] = [{}, {}]
 
     return tree
 
@@ -57,7 +57,49 @@ def delete_node(value, tree):
     :param tree: 
     :return: 
     """
-    pass
+    level = 0
+    if level == 0:
+        tree_bak = tree
+    for node in tree:
+        if value == node:
+            print("Find %s in the tree." % value)
+            if not tree[node][0] and not tree[node][1]:
+                print('=================')
+                print(tree)
+                tree_bak = {}
+            elif not tree[node][0] and tree[node][1]:
+                tree_bak = tree[node][1]
+            elif tree[node][0] and not tree[node][1]:
+                tree = tree[node][0]
+            elif tree[node][0] and tree[node][1]:
+                print('========2=========')
+
+                def restructure(temp_tree):
+                    tree_list = temp_tree[[i for i in tree.keys()][0]]
+                    flag = 0
+                    for i in range(len(tree_list)):
+                        sub_tree = tree_list[i]
+                        for sub_node in sub_tree:
+                            if not sub_tree[sub_node][1-i]:     # 解释：以a表示被删除节点，b、c分别表示a的左右两个子节点。当选择左边的子节点b替换被删除节点a时，子节点b的右子节点需为空，以将子节点c插在b的右子节点位置上；选择右边亦同理。所以用1-i来进行交叉判断。
+                                sub_tree[sub_node][1 - i] = tree_list[1 - i]
+                            else:
+                                flag += 1
+                                if flag >= 2:
+                                    flag = 0
+                                    restructure(sub_tree)
+                    return tree
+                restructure(tree)
+        elif value < node:
+            if not tree[node][0]:
+                print("Cannot delete %s because it is not in the tree." % value)
+            else:
+                delete_node(value, tree[node][0])
+        elif value > node:
+            if not tree[node][1]:
+                print("Cannot delete %s because it is not in the tree." % value)
+            else:
+                delete_node(value, tree[node][1])
+    return tree_bak
 
 
 def print_node(tree):
@@ -93,3 +135,7 @@ t6 = find_node(28, a)
 t7 = find_node(8, a)
 t8 = find_node(80, a)
 print(t1, t2, t3, t4, t5, t6, t7, t8)
+
+a = delete_node(87, a)
+print(a)
+print_node(a)

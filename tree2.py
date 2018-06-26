159e8f610a18c26311f305b5242e92680d58e8be
KingSuo <648230502@q.com>
2018-06-26 10:49:29 +0800

    fixed find_node

--- tree2.py
+++ tree2.py
@@ -6,21 +6,58 @@ def insert_node(value, tree={}):
     :return:
     """
     if tree:
-        for key in tree:
-            if value < key:
-                if tree[key][0] is None:
-                    tree[key][0] = {value: [None, None]}
+        for node in tree:
+            if value < node:
+                if tree[node][0] is None:
+                    tree[node][0] = {value: [None, None]}
                 else:
-                    insert_node3(value, tree[key][0])
-            elif value > key:
-                if tree[key][1] is None:
-                    tree[key][1] = {value: [None, None]}
+                    insert_node(value, tree[node][0])
+            elif value > node:
+                if tree[node][1] is None:
+                    tree[node][1] = {value: [None, None]}
                 else:
-                    insert_node3(value, tree[key][1])
+                    insert_node(value, tree[node][1])
     else:
         tree[value] = [None, None]
 
     return tree
+
+
+def find_node(value, tree):
+    """
+    查找节点
+    :param value: 
+    :param tree: 
+    :return: 
+    """
+    flag = 0
+    for node in tree:
+        if value == node:
+            print("Find %s in the tree." % value)
+            flag = 1
+        elif value < node:
+            if not tree[node][0]:
+                print("%s is not in the tree." % value)
+                flag = 0
+            else:
+                flag = find_node(value, tree[node][0])
+        elif value > node:
+            if not tree[node][1]:
+                print("%s is not in the tree." % value)
+                flag = 0
+            else:
+                flag = find_node(value, tree[node][1])
+    return flag
+
+
+def delete_node(value, tree):
+    """
+    删除节点
+    :param value: 
+    :param tree: 
+    :return: 
+    """
+    pass
 
 
 def print_node(tree):
@@ -36,3 +73,23 @@ def print_node(tree):
                 print_node(tree[i][0])
             if tree[i][1]:
                 print_node(tree[i][1])
+
+
+a = insert_node(35)
+insert_node(17, a)
+insert_node(39, a)
+insert_node(16, a)
+insert_node(56, a)
+insert_node(87, a)
+insert_node(28, a)
+print(a)
+print_node(a)
+t1 = find_node(35, a)
+t2 = find_node(56, a)
+t3 = find_node(39, a)
+t4 = find_node(16, a)
+t5 = find_node(87, a)
+t6 = find_node(28, a)
+t7 = find_node(8, a)
+t8 = find_node(80, a)
+print(t1, t2, t3, t4, t5, t6, t7, t8)
